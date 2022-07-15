76017b81360cfe3efe8f32e8132159199b78b511
Derik Figueiredo <derikfigueiredocontact@gmail.com>
2022-07-15 00:27:38 -0300

    closure

--- javascript_udemy/04-javascript-funcoes-avancado/70-retorno-da-funcao/main.js
+++ javascript_udemy/04-javascript-funcoes-avancado/70-retorno-da-funcao/main.js
@@ -1,4 +1,4 @@
-function falaFrase(comeco) {
+/*function falaFrase(comeco) {
     function falaResto(resto) {
         return comeco + ' ' + resto;
     }
@@ -8,3 +8,38 @@ function falaFrase(comeco) {
 const fala = falaFrase('Olá');
 const resto = fala('mundo')
 console.log(resto);
+*/
+
+
+//                   ESCOPO LÉXICO
+function init() {
+    var name = "Mozilla";
+    function displayName() {
+      alert(name);
+    }
+    displayName();
+  }
+  init();
+
+// A função init() cria uma variável local chamada name, e depois define uma função chamada displayName(). displayName() é uma função aninhada (um closure) — ela é definida dentro da função init(), e está disponivel apenas dentro do corpo daquela função. Diferente de init(), displayName() não tem variáveis locais próprias, e ao invés disso reusa a variável name declarada na função pai.
+
+//Este é um exemplo de escopo léxico: em JavaScript, o escopo de uma variável é definido por sua localização dentro do código fonte (isto é aparentemente léxico) e funções aninhadas têm acesso às variáveis declaradas em seu escopo externo.
+
+
+
+//                        CLOSURE
+
+//Closure é a forma de fazer com que as variáveis dentro de uma função sejam privadas e persistentes.
+
+function makeFunc() {
+    var name = "Mozilla";
+    function displayName() {
+      alert(name);
+    }
+    return displayName;
+  }
+  
+  var myFunc = makeFunc();
+  myFunc();
+
+//A solução para tal problema é que a função myFunc tornou-se uma closure. Uma closure (fechamento) trata-se de um tipo especial de objeto que combina duas coisas: a função e o ambiente onde a função foi criada. Este ambiente consiste de quaisquer variáveis que estavam no escopo naquele momento em que a função foi criada. Neste caso, myFunc é a closure que incorpora tanto a função displayName quanto a palavra Mozilla que existia quando a closure foi criada.
--- javascript_udemy/05-javascript_arrays_avancado/revisao_basica_arrays/main.js
+++ javascript_udemy/05-javascript_arrays_avancado/revisao_basica_arrays/main.js
@@ -3,26 +3,52 @@ let nomes = ['Eduardo', 'Maria', 'Joana'];
  //Apagar elemento e deixar um espaço vazio no lugar dele
 delete nomes[2];
 console.log(nomes);
+//OUTPUT: [ 'Eduardo', 'Maria', <1 empty item> ]
 
-//Alterar elemento
-nomes[2] = 'Luiz';
+//                     ARRAY LITERAL
+const arrayLiteral = new Array('FFF', 'GGG');
+
+
+
+//                   VALOR POR REFERÊNCIA
+//Valores por referência = tudo que eu fizer em um, efeta outro
+const novo = nomes;
+//o que eu mudar em na variável novo, altera a variável nomes e vice-versa.
+
+novo.pop();
 console.log(nomes);
+//OUTPUT: [ 'Eduardo', 'Maria' ]
 
-//Valor por referência
-const novos = nomes; //o que eu mudar em novos, irá mudar em nomes também e vice versa
 
-//Para não ocorre referência entre duas variáveis
-let novos2 = [...nomes];
-novos2[3] = "diogo";
-console.log(novos2);
 
-//Pop, retornar o elemento removido, trabalha no final do array
-const removido = nomes.pop();
-console.log(removido);
+//             COMO CONSERTAR ESSA QUESTÃO DE REFERÊNCIA?
+let nomes2 = ['Eduardo', 'Maria', 'Joana'];
+const novo2 = [...nomes2];
 
-//shift, funciona como a pop, shilt
- const removido2 = nomes.shift()
-    console.log(nomes, removido2);
+let exNomes = nomes2.pop();
+console.log(nomes2, exNomes);
+//OUTPUT: [ 'Eduardo', 'Maria' ] Joana
+console.log(novo2);
+//OUTPUT: [ 'Eduardo', 'Maria', 'Joana' ]
 
-//
-nomes.push('João')
+
+
+
+//                 LENGTH
+console.log(nomes2.length);
+//length não é um método e sim um atributo
+
+
+//              SHIFT
+//SHIFT remove do começo
+const removidoShift = nomes2.shift();
+console.log(nomes2, removidoShift);
+//OUTPUT: [ 'Maria' ] Eduardo
+
+
+//           PUSH 
+
+
+
+
+//https://www.freecodecamp.org/news/manipulating-arrays-in-javascript/
